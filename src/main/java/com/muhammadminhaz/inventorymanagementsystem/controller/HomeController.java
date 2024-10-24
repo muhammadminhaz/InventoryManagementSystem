4ae1a321e811a8d5a8ea006476f40108b0972cd7
Minhaz <mminhazurrahman0@gmail.com>
2024-10-24 13:52:00 +0600

    home update

--- src/main/java/com/muhammadminhaz/inventorymanagementsystem/controller/HomeController.java
+++ src/main/java/com/muhammadminhaz/inventorymanagementsystem/controller/HomeController.java
@@ -1,12 +1,29 @@
 package com.muhammadminhaz.inventorymanagementsystem.controller;
 
+import com.muhammadminhaz.inventorymanagementsystem.service.DashboardService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
 
 @Controller
 public class HomeController {
-    @GetMapping("/")
-    public String home() {
-        return "dashboard";
+
+    private final DashboardService dashboardService;
+
+    public HomeController(DashboardService dashboardService) {
+        this.dashboardService = dashboardService;
     }
+
+    @GetMapping("/")
+    public String home(Model model) {
+        Map<String, Object> salesData = dashboardService.salesData();
+        Object sales = salesData.get("sales");
+        Object dates = salesData.get("dates");
+
+        model.addAttribute("salesData", sales);
+        model.addAttribute("dates", dates);
+
+        return "dashboard";    }
 }
