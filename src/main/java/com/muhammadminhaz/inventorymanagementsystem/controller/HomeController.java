6303c853704acdf70d49cacd39f2869124d9cbb9
Minhaz <mminhazurrahman0@gmail.com>
2024-11-26 19:41:18 +0600

    login redirection

--- src/main/java/com/muhammadminhaz/inventorymanagementsystem/controller/HomeController.java
+++ src/main/java/com/muhammadminhaz/inventorymanagementsystem/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.muhammadminhaz.inventorymanagementsystem.controller;
 
 import com.muhammadminhaz.inventorymanagementsystem.service.DashboardService;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +13,10 @@ public class HomeController {
 
 
     @GetMapping("/")
-    public String home() {
+    public String home(Authentication authentication) {
+        if (authentication != null && authentication.isAuthenticated()) {
+            return "redirect:/dashboard"; // Redirect to dashboard if user is already logged in
+        }
         return "home";
     }
 }
